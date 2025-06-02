e686287785838ab6948c14a4d1034d3a234a7ca4
iii <isaacmartinez@MacBook-Pro-de-Isaac-2.local>
2025-06-01 22:09:14 -0600

    pregunta 4

--- lib/jswatch_web/statecharts/clock_manager.ex
+++ lib/jswatch_web/statecharts/clock_manager.ex
@@ -5,210 +5,217 @@ defmodule JswatchWeb.ClockManager do
 
   alias JswatchWeb.Endpoint, as: UI
 
-  @second_in_ms 1000
-  @blink_interval 250
-  @long_press_duration 250
-  @max_blink_cycles 20
+  @update_interval 1000
+  @long_press_threshold 250
+  @blink_cycle_duration 250
+  @max_blink_repetitions 20
 
   @impl true
-  def init(ui_pid) do
+  def init(display_pid) do
     :gproc.reg({:p, :l, :ui_event})
-    {_, native_time} = :calendar.local_time()
-    current_time = Time.from_erl!(native_time)
-    default_alarm = Time.add(current_time, 10)
+    {_, current_erl_time} = :calendar.local_time()
+    current_time_val = Time.from_erl!(current_erl_time)
+    default_alarm_val = Time.add(current_time_val, 10)
 
-    Process.send_after(self(), :clock_tick, @second_in_ms)
-    {:ok,
-     %{
-       ui_pid: ui_pid,
-       current_time: current_time,
-       alarm_time: default_alarm,
-       state_mode: :DisplayingTime,
-       active_mode: :Time,
-       last_interaction_at: nil,
-       is_visible: false,
-       blink_counter: 0,
-       editing_value: nil,
-       current_selection: nil,
-       is_button_held: false,
-       hold_timer_ref: nil
-     }}
+    Process.send_after(self(), :clock_pulse, @update_interval)
+    {:ok, %{
+      ui_pid: display_pid,
+      main_time: current_time_val,
+      alarm_setting: default_alarm_val,
+      activity_mode: :Viewing,
+      display_mode: :Clock,
+      last_event_time: nil,
+      is_display_on: false,
+      blink_iteration: 0,
+      temp_edit_time: nil,
+      active_segment: nil,
+      is_key_down: false,
+      key_press_timer: nil
+    }}
   end
 
   @impl true
-  def handle_info(event, state) do
-    case event do
-      :clock_tick -> handle_time_progression(state)
-      :"bottom-right-pressed" -> handle_button_press(:right, state)
-      :"bottom-left-pressed" -> handle_button_press(:left, state)
-      :"bottom-right-released" -> handle_button_release(:right, state)
-      :"bottom-left-released" -> handle_button_release(:left, state)
-      {:check_hold, press_start, action_type} -> handle_hold_check(press_start, action_type, state)
-      :editing_blink -> handle_display_blink(state)
-      :exit_editing_mode -> handle_editing_exit(state)
-      _ -> {:noreply, state}
+  def handle_info(message, state) do
+    case message do
+      :clock_pulse -> process_clock_tick(state)
+      :"bottom-right-pressed" -> handle_button_input(:right_down, state)
+      :"bottom-left-pressed" -> handle_button_input(:left_down, state)
+      :"bottom-right-released" -> handle_button_input(:right_up, state)
+      :"bottom-left-released" -> handle_button_input(:left_up, state)
+      {:check_hold_duration, press_start, edit_kind} -> verify_button_hold(press_start, edit_kind, state)
+      :blink_animation -> update_editing_display(state)
+      :finalize_edit -> exit_editing_state(state)
+      :clock_pulse when state.activity_mode == :Editing -> {:noreply, state}
+      :"bottom-right-pressed" when state.activity_mode == :Editing -> {:noreply, state}
+      :"bottom-left-pressed" when state.activity_mode == :Editing -> {:noreply, state}
+      {:check_hold_duration, _press_start, _edit_type} when state.activity_mode == :Editing -> {:noreply, state}
+      :"top-right-pressed" -> {:noreply, state}
+      :"top-right-released" -> {:noreply, state}
+      :"top-left-pressed" -> {:noreply, state}
+      :"top-left-released" -> {:noreply, state}
+      :"bottom-right-pressed" -> {:noreply, state}
+      {:check_hold_duration, _press_start, _edit_type} when state.activity_mode not in [:Editing] -> {:noreply, state}
+      :start_alarm -> {:noreply, state}
+      _ -> raise "Unexpected message in manager: #{inspect(message)}"
     end
   end
 
-  defp handle_time_progression(%{ui_pid: ui, current_time: time, state_mode: current_state, active_mode: active_mode, alarm_time: alarm} = state) do
-    Process.send_after(self(), :clock_tick, @second_in_ms)
+  defp process_clock_tick(%{ui_pid: display_pid, main_time: time, activity_mode: current_activity, display_mode: current_display, alarm_setting: alarm_val} = state) do
+    Process.send_after(self(), :clock_pulse, @update_interval)
 
-    updated_time = Time.add(time, 1)
+    new_time = Time.add(time, 1)
 
-    should_show_time =
-      case {current_state, active_mode} do
-        {:DisplayingTime, _} -> true
+    should_update_display =
+      case {current_activity, current_display} do
+        {:Viewing, _} -> true
         {:Editing, :Alarm} -> false
-        {:Editing, :Time} -> false
+        {:Editing, :Clock} -> false
         _ -> false
       end
 
-    if should_show_time do
-      send_time_to_ui(ui, updated_time)
+    if should_update_display do
+      update_ui_time(display_pid, new_time)
     end
 
-    if updated_time == alarm && current_state == :DisplayingTime do
+    if new_time == alarm_val and current_activity == :Viewing do
       :gproc.send({:p, :l, :ui_event}, :start_alarm)
     end
 
-    {:noreply, %{state | current_time: updated_time}}
+    {:noreply, %{state | main_time: new_time}}
   end
 
-  defp handle_button_press(button_side, %{state_mode: :DisplayingTime, is_button_held: false} = state) do
-    monotonic_now = System.monotonic_time(:millisecond)
-    action = if button_side == :right, do: :edit_clock, else: :edit_alarm
-    Process.send_after(self(), {:check_hold, monotonic_now, action}, @long_press_duration)
-    {:noreply, %{state | is_button_held: true, hold_timer_ref: monotonic_now}}
+  defp handle_button_input(:right_down, %{activity_mode: :Viewing, is_key_down: false} = state) do
+    current_mono_time = System.monotonic_time(:millisecond)
+    Process.send_after(self(), {:check_hold_duration, current_mono_time, :time_adjust}, @long_press_threshold)
+    {:noreply, %{state | is_key_down: true, key_press_timer: current_mono_time}}
   end
 
-  defp handle_button_press(_button_side, state) do
-    {:noreply, state}
+  defp handle_button_input(:left_down, %{activity_mode: :Viewing, is_key_down: false} = state) do
+    current_mono_time = System.monotonic_time(:millisecond)
+    Process.send_after(self(), {:check_hold_duration, current_mono_time, :alarm_adjust}, @long_press_threshold)
+    {:noreply, %{state | is_key_down: true, key_press_timer: current_mono_time}}
   end
 
-  defp handle_button_release(_button_side, state) do
-    {:noreply, %{state | is_button_held: false, hold_timer_ref: nil}}
+  defp handle_button_input(:right_up, state), do: {:noreply, %{state | is_key_down: false, key_press_timer: nil}}
+  defp handle_button_input(:left_up, state), do: {:noreply, %{state | is_key_down: false, key_press_timer: nil}}
+
+  defp handle_button_input(:right_down, %{activity_mode: :Editing, active_segment: current_segment} = state) do
+    next_segment =
+      case current_segment do
+        :hour -> :minute
+        :minute -> :second
+        :second -> :hour
+      end
+    {:noreply, %{state | active_segment: next_segment, blink_iteration: 0}}
   end
 
-  defp handle_hold_check(press_start, action_type, %{is_button_held: true, hold_timer_ref: ^press_start, state_mode: :DisplayingTime, current_time: time, alarm_time: alarm, ui_pid: ui} = state) do
-    stopwatch_active = case :gproc.lookup_values({:p, :l, :stopwatch_active}) do
+  defp handle_button_input(:left_down, %{activity_mode: :Editing, temp_edit_time: edit_time, active_segment: segment, ui_pid: display_pid} = state) do
+    adjusted_time = increment_time_part(edit_time, segment)
+    update_ui_time(display_pid, adjusted_time)
+    {:noreply, %{state | temp_edit_time: adjusted_time, blink_iteration: 0}}
+  end
+
+  defp verify_button_hold(press_start_time, adjustment_type, %{is_key_down: true, key_press_timer: ^press_start_time, activity_mode: :Viewing, main_time: time_val, alarm_setting: alarm_val, ui_pid: display_pid} = state) do
+    is_stopwatch_running = case :gproc.lookup_values({:p, :l, :stopwatch_active}) do
       [] -> false
       _ -> true
     end
 
-    if stopwatch_active do
+    if is_stopwatch_running do
       {:noreply, state}
     else
       if :gproc.lookup_values({:p, :l, :edit_mode}) == [] do
         :gproc.reg({:p, :l, :edit_mode})
       end
 
-      case action_type do
-        :edit_clock ->
-          GenServer.cast(ui, {:set_time_display, "EDITING"})
-          Process.send_after(self(), :editing_blink, @blink_interval)
+      case adjustment_type do
+        :time_adjust ->
+          GenServer.cast(display_pid, {:set_time_display, "EDITING"})
+          Process.send_after(self(), :blink_animation, @blink_cycle_duration)
 
-          {:noreply,
-           %{
-             state
-             | state_mode: :Editing,
-               active_mode: :Time,
-               current_selection: :hour,
-               is_visible: true,
-               blink_counter: 0,
-               editing_value: time,
-               last_interaction_at: nil
-           }}
+          {:noreply, %{
+            state
+            | activity_mode: :Editing,
+              display_mode: :Clock,
+              active_segment: :hour,
+              is_display_on: true,
+              blink_iteration: 0,
+              temp_edit_time: time_val,
+              last_event_time: nil
+          }}
 
-        :edit_alarm ->
-          GenServer.cast(ui, {:set_time_display, "EDIT ALARM"})
-          Process.send_after(self(), :editing_blink, @blink_interval)
+        :alarm_adjust ->
+          GenServer.cast(display_pid, {:set_time_display, "EDIT ALARM"})
+          Process.send_after(self(), :blink_animation, @blink_cycle_duration)
 
-          {:noreply,
-           %{
-             state
-             | state_mode: :Editing,
-               active_mode: :Alarm,
-               current_selection: :hour,
-               is_visible: true,
-               blink_counter: 0,
-               editing_value: alarm,
-               last_interaction_at: nil
-           }}
+          {:noreply, %{
+            state
+            | activity_mode: :Editing,
+              display_mode: :Alarm,
+              active_segment: :hour,
+              is_display_on: true,
+              blink_iteration: 0,
+              temp_edit_time: alarm_val,
+              last_event_time: nil
+          }}
       end
     end
   end
 
-  defp handle_hold_check(_press_start, _action_type, state), do: {:noreply, state}
+  defp verify_button_hold(_press_start, _adjust_type, state), do: {:noreply, state}
 
-  defp handle_display_blink(%{state_mode: :Editing, is_visible: visible, ui_pid: ui, editing_value: edit_value, current_selection: selection, blink_counter: counter} = state) do
-    new_visible = !visible
-    new_counter = if new_visible, do: counter + 1, else: counter
+  defp update_editing_display(%{activity_mode: :Editing, is_display_on: current_on, ui_pid: display_pid, temp_edit_time: current_edit_val, active_segment: segment, blink_iteration: iter_count} = state) do
+    new_on_status = !current_on
+    next_iteration = if new_on_status, do: iter_count + 1, else: iter_count
 
-    if new_counter == @max_blink_cycles do
-      send(self(), :exit_editing_mode)
+    if next_iteration == @max_blink_repetitions do
+      send(self(), :finalize_edit)
       {:noreply, state}
     else
-      {h, m, s} = {edit_value.hour, edit_value.minute, edit_value.second}
+      {h, m, s} = {current_edit_val.hour, current_edit_val.minute, current_edit_val.second}
 
-      display_str =
-        case selection do
-          :hour -> "#{if(new_visible, do: pad_zero(h), else: "  ")}:#{pad_zero(m)}:#{pad_zero(s)}"
-          :minute -> "#{pad_zero(h)}:#{if(new_visible, do: pad_zero(m), else: "  ")}:#{pad_zero(s)}"
-          :second -> "#{pad_zero(h)}:#{pad_zero(m)}:#{if(new_visible, do: pad_zero(s), else: "  ")}"
+      display_output =
+        case segment do
+          :hour -> "#{if(new_on_status, do: format_num(h), else: "  ")}:#{format_num(m)}:#{format_num(s)}"
+          :minute -> "#{format_num(h)}:#{if(new_on_status, do: format_num(m), else: "  ")}:#{format_num(s)}"
+          :second -> "#{format_num(h)}:#{format_num(m)}:#{if(new_on_status, do: format_num(s), else: "  ")}"
         end
 
-      GenServer.cast(ui, {:set_time_display, display_str})
-      Process.send_after(self(), :editing_blink, @blink_interval)
+      GenServer.cast(display_pid, {:set_time_display, display_output})
+      Process.send_after(self(), :blink_animation, @blink_cycle_duration)
 
-      {:noreply, %{state | is_visible: new_visible, blink_counter: new_counter}}
+      {:noreply, %{state | is_display_on: new_on_status, blink_iteration: next_iteration}}
     end
   end
 
-  defp handle_display_blink(state), do: {:noreply, state}
+  defp update_editing_display(state), do: {:noreply, state}
 
-  defp handle_button_press(:right, %{state_mode: :Editing, current_selection: selection} = state) do
-    next_selection =
-      case selection do
-        :hour -> :minute
-        :minute -> :second
-        :second -> :hour
-      end
-
-    {:noreply, %{state | current_selection: next_selection, blink_counter: 0}}
-  end
-
-  defp handle_button_press(:left, %{state_mode: :Editing, editing_value: edit_value, current_selection: selection, ui_pid: ui} = state) do
-    adjusted_edit_time = adjust_time_segment(edit_value, selection)
-    send_time_to_ui(ui, adjusted_edit_time)
-    {:noreply, %{state | editing_value: adjusted_edit_time, blink_counter: 0}}
-  end
-
-  defp handle_editing_exit(%{state_mode: :Editing, editing_value: edited_value, ui_pid: ui, active_mode: mode} = state) do
-    send_time_to_ui(ui, edited_value)
+  defp exit_editing_state(%{activity_mode: :Editing, temp_edit_time: final_value, ui_pid: display_pid, display_mode: mode_being_edited} = state) do
+    update_ui_time(display_pid, final_value)
     :gproc.unreg({:p, :l, :edit_mode})
 
-    final_state =
-      case mode do
-        :Time ->
-          %{state | state_mode: :DisplayingTime, current_time: edited_value, editing_value: nil, current_selection: nil, is_visible: false, blink_counter: 0, active_mode: :Time}
+    result_state =
+      case mode_being_edited do
+        :Clock ->
+          %{state | activity_mode: :Viewing, main_time: final_value, temp_edit_time: nil, active_segment: nil, is_display_on: false, blink_iteration: 0, display_mode: :Clock}
 
         :Alarm ->
-          %{state | state_mode: :DisplayingTime, alarm_time: edited_value, editing_value: nil, current_selection: nil, is_visible: false, blink_counter: 0, active_mode: :Time}
+          %{state | activity_mode: :Viewing, alarm_setting: final_value, temp_edit_time: nil, active_segment: nil, is_display_on: false, blink_iteration: 0, display_mode: :Clock}
       end
 
-    {:noreply, final_state}
+    {:noreply, result_state}
   end
 
-  defp adjust_time_segment(time, :hour), do: %{time | hour: rem(time.hour + 1, 24)}
-  defp adjust_time_segment(time, :minute), do: %{time | minute: rem(time.minute + 1, 60)}
-  defp adjust_time_segment(time, :second), do: %{time | second: rem(time.second + 1, 60)}
+  defp increment_time_part(time, :hour), do: %{time | hour: rem(time.hour + 1, 24)}
+  defp increment_time_part(time, :minute), do: %{time | minute: rem(time.minute + 1, 60)}
+  defp increment_time_part(time, :second), do: %{time | second: rem(time.second + 1, 60)}
 
-  defp pad_zero(n) when n < 10, do: "0#{n}"
-  defp pad_zero(n), do: "#{n}"
+  defp format_num(n) when n < 10, do: "0#{n}"
+  defp format_num(n), do: "#{n}"
 
-  defp send_time_to_ui(ui_pid, time_val) do
-    formatted_time = Time.truncate(time_val, :second) |> Time.to_string()
-    GenServer.cast(ui_pid, {:set_time_display, formatted_time})
+  defp update_ui_time(display_pid, time_value) do
+    formatted_value = Time.truncate(time_value, :second) |> Time.to_string()
+    GenServer.cast(display_pid, {:set_time_display, formatted_value})
   end
 
   def start_link(args) do
